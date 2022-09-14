13d9aa5feb4c87e30a8ae4f7a15f050e00102804
Harshad Hegde <hegdehb@gmail.com>
2022-09-14 14:47:39 -0500

    added files from the old repo

---
+++ tests/test_cli.py
@@ -0,0 +1,41 @@
+import logging
+import unittest
+
+from click.testing import CliRunner
+import kgcl_schema.grammer.parser as kgcl_parser
+
+from kgcl_schema.datamodel.kgcl_schema import Session
+from kgcl_schema.utils import from_yaml
+
+from tests import TMP_YAML
+from tests.cases import CASES, TODO_TOKEN, UID
+
+
+class CliTestSuite(unittest.TestCase):
+    """
+    Tests command line interfaces
+    """
+
+    def setUp(self) -> None:
+        runner = CliRunner(mix_stderr=False)
+        self.runner = runner
+
+    def test_parse_help(self):
+        result = self.runner.invoke(kgcl_parser.cli, ["--help"])
+        out = result.stdout
+        self.assertEqual(0, result.exit_code)
+
+    def test_parse_multi(self):
+        patches = [t[0] for t in CASES]
+        objs = [t[2] for t in CASES]
+        result = self.runner.invoke(kgcl_parser.cli, patches + ["-o", TMP_YAML])
+        session: Session = from_yaml(TMP_YAML)
+        self.assertEqual(len(patches), len(session.change_set))
+        #for ch in session.change_set:
+        #    ch.id = UID
+        #    self.assertIn(ch, objs)
+        out = result.stdout
+        self.assertEqual(0, result.exit_code)
+
+
+
